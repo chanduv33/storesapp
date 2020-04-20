39e898f1c861e1879d069f639b465b384e709b60
chanduv33 <chandu.teja.0557@gmail.com>
2020-04-20 13:53:13 +0530

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'storesmanagementsystem'`, () => {
+  it(`should have as title 'storesmanagementapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('storesmanagementsystem');
+    expect(app.title).toEqual('storesmanagementapp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('storesmanagementsystem app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('storesmanagementapp app is running!');
   });
 });
